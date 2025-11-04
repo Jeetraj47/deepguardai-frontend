cfc8e53897911e69c2db45bbc031b3180380f615
jeet raj <160517388+Jeetraj47@users.noreply.github.com>
2025-11-04 07:03:24 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // DeepGuardAI - Main JavaScript
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "https://deepguardai-backend.onrender.com";
 
 // Get token from localStorage
 function getToken() {
